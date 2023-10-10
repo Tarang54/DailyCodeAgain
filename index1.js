d6cc57499f7199387a40491353dfee1667c48383
Tarang Sultania <108229445+Tarang54@users.noreply.github.com>
2023-10-10 22:14:28 +0530

    day5 uploaded

---
+++ index1.js
@@ -0,0 +1,9 @@
+let a = btn1.addEventListener("mouseover",function(event){
+    alert("Button 1 got clicked");
+});
+let b = btn2.addEventListener("click",function(event){
+    alert("Button 2 got clicked");
+});
+let c = btn3.addEventListener("click",function(event){
+    alert("Button 3 got clicked");
+});
